25b10b7abb20b0d6d8546ae1bc7d177e714c4558
ibrasil <12400718@aluno.cotemig.com.br>
2026-05-27 09:06:36 -0300

    Add files via upload

---
+++ Aula3/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/sobre/<nome>')
+def sobre(nome):
+    return f'Olá, {nome}! Bem-vindo(a) à pagina.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
